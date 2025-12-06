c143bc1f0dfd196e5803ccfa1d6458c006d2cf0b
SarbBhangu <Bhangu513@gmail.com>
2025-12-06 10:00:34 -0500

    added Character Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,3 @@
-import TextInput from "./components/TextInput/TextInput";
 
 
-function App() {
-  function handleTextChange(text: string) {
-    console.log("User typed:", text);
-  }
-
-  return (
-    <div>
-      <h1>TextInput Test</h1>
-      <TextInput onTextChange={handleTextChange} />
-    </div>
-  );
-}
-
-export default App;
 
---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,9 @@
+import type { TextStats } from "../../types";
+import { useState } from "react";
+import TextInput from "../TextInput/TextInput";
+import StatsDisplay from "../StatsDisplay/StatsDisplay";
+
+
+
+
+
